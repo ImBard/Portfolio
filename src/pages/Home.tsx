2e3c61f6a6c76166fc830956f75e6351ca99c550
ImBard <nicota1234@gmail.com>
2022-09-07 02:27:44 -0300

    :construction: Main header, repository, function get repo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
 import "./styles.css"
 import { Side } from "../components/side"
+import axios from "axios"
+import { useState } from "react"
+
+interface Repository {
+  name: string;
+  id: string;
+}
 
 export function Home() {
+
+  const [repos, setRepos] = useState<Repository[]>([]);
+
+  axios.get('https://api.github.com/users/imbard/repos')
+  .then((response) => {
+    setRepos(response.data)
+  })
+
   return (
     <div className="body">
       <Side />
 
       <main className="main">
-        main
+        <header className="My-Projects">
+          My Projects
+        </header>
+        <ul>
+          <li>{repos[2].name}</li>
+          <li>{repos[3].name}</li>
+        
+        </ul>
       </main>
     </div>
   )

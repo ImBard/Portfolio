2736e24d2dced53c1bbeab4a58196268dd06ed99
ImBard <nicota1234@gmail.com>
2022-09-08 19:46:50 -0300

    :sparkles: feat: Repos Card finalized

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,64 @@
-import "./styles.css"
-import { Side } from "../components/side"
-import axios from "axios"
-import { useState } from "react"
+import "./styles.css";
+import { Side } from "../components/side";
+import axios from "axios";
+import { useState } from "react";
+import { FolderSimple, GitFork, List, Star } from "phosphor-react";
 
 interface Repository {
   name: string;
   id: string;
+  html_url: string;
+  description: string;
+  stargazers_count: number;
+  language: string;
+  forks_count: number;
 }
 
 export function Home() {
+  const [repos, setRepos] = useState<Repository[]>();
 
-  const [repos, setRepos] = useState<Repository[]>([]);
+  const [showSide, setShowSide] = useState<boolean>(false)
 
-  axios.get('https://api.github.com/users/imbard/repos')
-  .then((response) => {
-    setRepos(response.data)
-  })
+  axios.get("https://api.github.com/users/imbard/repos").then((response) => {
+    setRepos(response.data);
+  });
 
   return (
     <div className="body">
-      <Side />
+      <button onClick={() => setShowSide(!showSide)} className="button_tab">
+        <List size={32} />
+      </button>
+      <Side display={showSide}/>
 
       <main className="main">
-        <header className="My-Projects">
-          My Projects
-        </header>
-        <ul>
-          <li>{repos[2].name}</li>
-          <li>{repos[3].name}</li>
-        
+        <header className="My-Projects">My Projects</header>
+        <ul className="ul">
+          {repos?.map((item) => {
+            return (
+              <a className="repo_a" href={item.html_url}>
+                <li className="li" key={item.id}>
+                  <h1 className="title_repo">
+                    <FolderSimple size={22} /> {item.name}
+                  </h1>
+
+                  <p className="desc">{item.description}</p>
+
+                  <div className="details_repo">
+                    <span>
+                      <Star size={22} />
+                      {item.stargazers_count}
+
+                      <GitFork size={22} />
+                      {item.forks_count}
+                    </span>
+                    <span className="fds">{item.language}</span>
+                  </div>
+                </li>
+              </a>
+            );
+          })}
         </ul>
       </main>
     </div>
-  )
+  );
 }
